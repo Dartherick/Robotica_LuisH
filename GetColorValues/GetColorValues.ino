64efd86c262cf14283f11d9e681de99feb5daccb
Dartherick <62523859+Dartherick@users.noreply.github.com>
2023-05-10 22:18:18 -0400

    Update GetColorValues.ino

--- GetColorValues/GetColorValues.ino
+++ GetColorValues/GetColorValues.ino
@@ -1,25 +1,36 @@
 int R,G,B;
 String Colors;
+String StringRead;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  GetColorValues();
 }
 
 void GetColorValues(){
-  int i1 = Colors.indexOf(',');
-  int i2 = Colors.indexOf(',', i1+1);
-  Serial.println(i1);
-  Serial.println(i2);
 
-  R = Colors.substring(0, i1).toInt();
-  G = Colors.substring(i1 + 1, i2).toInt();
-  B = Colors.substring(i2 + 1).toInt();
+  while (Serial.available())
+  {
+    StringRead = Serial.read();
+    if (StringRead.length() >0) 
+    {
+      Serial.println(StringRead); //see what was received
+      int i1 = Colors.indexOf(',');
+      int i2 = Colors.indexOf(',', i1+1);
+      Serial.println(i1);
+      Serial.println(i2);
 
-  Serial.println(R);
-  Serial.println(G);
-  Serial.println(B);
+      R = Colors.substring(0, i1).toInt();
+      G = Colors.substring(i1 + 1, i2).toInt();
+      B = Colors.substring(i2 + 1).toInt();
+
+      Serial.println("Valores obtenidos son:");
+      Serial.println(R);
+      Serial.println(G);
+      Serial.println(B);
+    }
+  }
 }
